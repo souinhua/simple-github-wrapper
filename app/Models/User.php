eb2aaf39cb171183fcf5351b90e3ddbdc45de2f3
Janssen Canturias <janssen@aloware.com>
2021-09-08 15:40:53 +0800

    :open_file_folder: Models

---
+++ app/Models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\NewAccessToken;
+use Laravel\Sanctum\PersonalAccessToken;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Deletes all the old tokens and creates a new one
+     *
+     * @return string
+     */
+    public function getPlainTextToken(): string {
+        $tokens = $this->tokens();
+
+        // delete old tokens if there are any
+        if ($tokens) {
+            $tokens->delete();
+        }
+
+        return $this->createToken('api')->plainTextToken;
+    }
+}
